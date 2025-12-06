94cb80416d01097d4b8f91b988154e66f027108c
shree-ml-journey0 <smutha0909@gmail.com>
2025-12-06 13:06:01 +0530

    Implement arithmetic and comparison operations
    Added arithmetic and comparison operations for two variables.

---
+++ Python_practice.py
@@ -0,0 +1,22 @@
+a = 25
+b = 44
+# arithematuc operators
+
+print((a + b))
+print((a - b))
+print((a * b))
+print((a / b))
+print((a % b))
+print((a ** b))
+
+        #arithemetric operators 
+        
+a = 25
+b = 45
+
+print((a == b))
+print(( a != b))
+print((a >= b))
+print((a <= b))
+print((a < b))
+print((a > b))
